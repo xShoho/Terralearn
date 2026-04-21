996cd93efbd89b1475bfaac1ab69b251a483b7b0
Kamil Starosta <ks@g3w.eu>
2026-04-21 19:33:44 +0200

    Started configuring ecs service
    	TODO: ALB target group

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -37,3 +37,14 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
     }
   ])
 }
+
+resource "aws_ecs_service" "ecs_service" {
+    name = "nginx"
+    cluster = aws_ecs_cluster.ecs_cluster.id
+    task_definition = aws_ecs_task_definition.ecs_task_definition.arn
+    desired_count = 2
+
+    # TODO AWS lb target group
+    # load_balancer {
+    # }
+}
