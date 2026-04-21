43d511ab6f8b381475b6e763fa4afe72bfab67d5
Kamil Starosta <ks@g3w.eu>
2026-04-21 19:47:40 +0200

    Added listener and target group to load balancer

--- terraform/load_balancer.tf
+++ terraform/load_balancer.tf
@@ -7,3 +7,22 @@ resource "aws_lb" "lb" {
 
   enable_deletion_protection = false
 }
+
+resource "aws_lb_listener" "lb_listener" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lb_target_group.arn
+  }
+}
+
+resource "aws_lb_target_group" "lb_target_group" {
+  name        = "terralearn-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.vpc.id
+  target_type = "ip"
+}
