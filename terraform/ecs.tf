0b966ccdf272cac41346450d2a4f38ef00ea8bb7
Kamil Starosta <ks@g3w.eu>
2026-04-21 19:23:28 +0200

    Added ecs task definition with fargate

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -6,3 +6,32 @@ resource "aws_ecs_cluster" "ecs_cluster" {
     value = "enabled"
   }
 }
+
+resource "aws_ecs_task_definition" "ecs_task_definition" {
+  family                   = "terralearn-containers"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "nginx"
+      image     = "${var.ecr_image}"
+      essential = true
+      portMappings = [{
+        containerPort = 80
+        hostPort      = 80
+      }]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.ecs_log_group.name
+          awslogs-region        = var.region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+}
